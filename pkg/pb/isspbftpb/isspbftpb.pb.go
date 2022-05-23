15e3497af4f1afb7d4f8e2a7bb5981a8b9f59b8e
Matej Pavlovic <matopavlovic@gmail.com>
2022-05-23 15:36:47 +0200

    Add PBFT view change timer events
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- pkg/pb/isspbftpb/isspbftpb.pb.go
+++ pkg/pb/isspbftpb/isspbftpb.pb.go
@@ -656,6 +656,61 @@ func (x *ReqWaitReference) GetView() uint64 {
 	return 0
 }
 
+type VCBatchTimeout struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	View         uint64 `protobuf:"varint,1,opt,name=view,proto3" json:"view,omitempty"`
+	NumCommitted uint64 `protobuf:"varint,2,opt,name=numCommitted,proto3" json:"numCommitted,omitempty"`
+}
+
+func (x *VCBatchTimeout) Reset() {
+	*x = VCBatchTimeout{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_isspbftpb_isspbftpb_proto_msgTypes[10]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *VCBatchTimeout) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*VCBatchTimeout) ProtoMessage() {}
+
+func (x *VCBatchTimeout) ProtoReflect() protoreflect.Message {
+	mi := &file_isspbftpb_isspbftpb_proto_msgTypes[10]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use VCBatchTimeout.ProtoReflect.Descriptor instead.
+func (*VCBatchTimeout) Descriptor() ([]byte, []int) {
+	return file_isspbftpb_isspbftpb_proto_rawDescGZIP(), []int{10}
+}
+
+func (x *VCBatchTimeout) GetView() uint64 {
+	if x != nil {
+		return x.View
+	}
+	return 0
+}
+
+func (x *VCBatchTimeout) GetNumCommitted() uint64 {
+	if x != nil {
+		return x.NumCommitted
+	}
+	return 0
+}
+
 type Status struct {
 	state         protoimpl.MessageState
 	sizeCache     protoimpl.SizeCache
@@ -665,7 +720,7 @@ type Status struct {
 func (x *Status) Reset() {
 	*x = Status{}
 	if protoimpl.UnsafeEnabled {
-		mi := &file_isspbftpb_isspbftpb_proto_msgTypes[10]
+		mi := &file_isspbftpb_isspbftpb_proto_msgTypes[11]
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		ms.StoreMessageInfo(mi)
 	}
@@ -678,7 +733,7 @@ func (x *Status) String() string {
 func (*Status) ProtoMessage() {}
 
 func (x *Status) ProtoReflect() protoreflect.Message {
-	mi := &file_isspbftpb_isspbftpb_proto_msgTypes[10]
+	mi := &file_isspbftpb_isspbftpb_proto_msgTypes[11]
 	if protoimpl.UnsafeEnabled && x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -691,7 +746,7 @@ func (x *Status) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use Status.ProtoReflect.Descriptor instead.
 func (*Status) Descriptor() ([]byte, []int) {
-	return file_isspbftpb_isspbftpb_proto_rawDescGZIP(), []int{10}
+	return file_isspbftpb_isspbftpb_proto_rawDescGZIP(), []int{11}
 }
 
 var File_isspbftpb_isspbftpb_proto protoreflect.FileDescriptor
@@ -764,11 +819,15 @@ var file_isspbftpb_isspbftpb_proto_rawDesc = []byte{
 	0x69, 0x74, 0x52, 0x65, 0x66, 0x65, 0x72, 0x65, 0x6e, 0x63, 0x65, 0x12, 0x0e, 0x0a, 0x02, 0x73,
 	0x6e, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04, 0x52, 0x02, 0x73, 0x6e, 0x12, 0x12, 0x0a, 0x04, 0x76,
 	0x69, 0x65, 0x77, 0x18, 0x02, 0x20, 0x01, 0x28, 0x04, 0x52, 0x04, 0x76, 0x69, 0x65, 0x77, 0x22,
-	0x08, 0x0a, 0x06, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x42, 0x32, 0x5a, 0x30, 0x67, 0x69, 0x74,
-	0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x66, 0x69, 0x6c, 0x65, 0x63, 0x6f, 0x69, 0x6e,
-	0x2d, 0x70, 0x72, 0x6f, 0x6a, 0x65, 0x63, 0x74, 0x2f, 0x6d, 0x69, 0x72, 0x2f, 0x70, 0x6b, 0x67,
-	0x2f, 0x70, 0x62, 0x2f, 0x69, 0x73, 0x73, 0x70, 0x62, 0x66, 0x74, 0x70, 0x62, 0x62, 0x06, 0x70,
-	0x72, 0x6f, 0x74, 0x6f, 0x33,
+	0x48, 0x0a, 0x0e, 0x56, 0x43, 0x42, 0x61, 0x74, 0x63, 0x68, 0x54, 0x69, 0x6d, 0x65, 0x6f, 0x75,
+	0x74, 0x12, 0x12, 0x0a, 0x04, 0x76, 0x69, 0x65, 0x77, 0x18, 0x01, 0x20, 0x01, 0x28, 0x04, 0x52,
+	0x04, 0x76, 0x69, 0x65, 0x77, 0x12, 0x22, 0x0a, 0x0c, 0x6e, 0x75, 0x6d, 0x43, 0x6f, 0x6d, 0x6d,
+	0x69, 0x74, 0x74, 0x65, 0x64, 0x18, 0x02, 0x20, 0x01, 0x28, 0x04, 0x52, 0x0c, 0x6e, 0x75, 0x6d,
+	0x43, 0x6f, 0x6d, 0x6d, 0x69, 0x74, 0x74, 0x65, 0x64, 0x22, 0x08, 0x0a, 0x06, 0x53, 0x74, 0x61,
+	0x74, 0x75, 0x73, 0x42, 0x32, 0x5a, 0x30, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f,
+	0x6d, 0x2f, 0x66, 0x69, 0x6c, 0x65, 0x63, 0x6f, 0x69, 0x6e, 0x2d, 0x70, 0x72, 0x6f, 0x6a, 0x65,
+	0x63, 0x74, 0x2f, 0x6d, 0x69, 0x72, 0x2f, 0x70, 0x6b, 0x67, 0x2f, 0x70, 0x62, 0x2f, 0x69, 0x73,
+	0x73, 0x70, 0x62, 0x66, 0x74, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -783,7 +842,7 @@ func file_isspbftpb_isspbftpb_proto_rawDescGZIP() []byte {
 	return file_isspbftpb_isspbftpb_proto_rawDescData
 }
 
-var file_isspbftpb_isspbftpb_proto_msgTypes = make([]protoimpl.MessageInfo, 11)
+var file_isspbftpb_isspbftpb_proto_msgTypes = make([]protoimpl.MessageInfo, 12)
 var file_isspbftpb_isspbftpb_proto_goTypes = []interface{}{
 	(*Preprepare)(nil),        // 0: isspbftpb.Preprepare
 	(*Prepare)(nil),           // 1: isspbftpb.Prepare
@@ -795,11 +854,12 @@ var file_isspbftpb_isspbftpb_proto_goTypes = []interface{}{
 	(*QSetEntry)(nil),         // 7: isspbftpb.QSetEntry
 	(*PreprepareRequest)(nil), // 8: isspbftpb.PreprepareRequest
 	(*ReqWaitReference)(nil),  // 9: isspbftpb.ReqWaitReference
-	(*Status)(nil),            // 10: isspbftpb.Status
-	(*requestpb.Batch)(nil),   // 11: requestpb.Batch
+	(*VCBatchTimeout)(nil),    // 10: isspbftpb.VCBatchTimeout
+	(*Status)(nil),            // 11: isspbftpb.Status
+	(*requestpb.Batch)(nil),   // 12: requestpb.Batch
 }
 var file_isspbftpb_isspbftpb_proto_depIdxs = []int32{
-	11, // 0: isspbftpb.Preprepare.batch:type_name -> requestpb.Batch
+	12, // 0: isspbftpb.Preprepare.batch:type_name -> requestpb.Batch
 	6,  // 1: isspbftpb.ViewChange.p_set:type_name -> isspbftpb.PSetEntry
 	7,  // 2: isspbftpb.ViewChange.q_set:type_name -> isspbftpb.QSetEntry
 	3,  // 3: isspbftpb.SignedViewChange.view_change:type_name -> isspbftpb.ViewChange
@@ -939,6 +999,18 @@ func file_isspbftpb_isspbftpb_proto_init() {
 			}
 		}
 		file_isspbftpb_isspbftpb_proto_msgTypes[10].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*VCBatchTimeout); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_isspbftpb_isspbftpb_proto_msgTypes[11].Exporter = func(v interface{}, i int) interface{} {
 			switch v := v.(*Status); i {
 			case 0:
 				return &v.state
@@ -957,7 +1029,7 @@ func file_isspbftpb_isspbftpb_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_isspbftpb_isspbftpb_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   11,
+			NumMessages:   12,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
