1819a8b0e2c7e42cb9780f5db159ffeb82f74745
Matej Pavlovic <matopavlovic@gmail.com>
2022-06-15 13:42:41 +0200

    Move Status() function modules to Module
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- pkg/modules/passivemodule.go
+++ pkg/modules/passivemodule.go
@@ -2,7 +2,6 @@ package modules
 
 import (
 	"github.com/filecoin-project/mir/pkg/events"
-	"github.com/filecoin-project/mir/pkg/pb/statuspb"
 )
 
 type PassiveModule interface {
@@ -11,8 +10,4 @@ type PassiveModule interface {
 	// ApplyEvents applies a list of input events to the module, making it advance its state
 	// and return a (potentially empty) list of output events that the application of the input events results in.
 	ApplyEvents(events *events.EventList) (*events.EventList, error)
-
-	// Status returns the current state of the module.
-	// Mostly for debugging purposes.
-	Status() (s *statuspb.ProtocolStatus, err error)
 }
