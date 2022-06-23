65d3696c6241c32f9d017be28822b2306a2b048b
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2022-06-23 21:41:12 +0200

    Add tests for context management in DSL modules

--- pkg/util/sliceutil/sliceutil.go
+++ pkg/util/sliceutil/sliceutil.go
@@ -1,9 +1,11 @@
 package sliceutil
 
+import "golang.org/x/exp/constraints"
+
 // Repeat returns a slice with value repeated n times.
-func Repeat[T any](value T, n int) []T {
+func Repeat[T any, N constraints.Integer](value T, n N) []T {
 	arr := make([]T, n)
-	for i := 0; i < n; i++ {
+	for i := N(0); i < n; i++ {
 		arr[i] = value
 	}
 	return arr
