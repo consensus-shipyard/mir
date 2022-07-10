734fc0ac29e4033a99edd55feb90e12f30229769
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2022-07-10 23:17:03 +0200

    Add multisig collector prototype
    The prototype implements the desired interface and is supposed to
    work properly in the crash-stop model. However, it does not use
    a real persistent storage (and, hence, does not support node
    recovery).
    The prototype implements a Byzantine fault-tolerant protocol, but
    it is susceptible to DOS attacks from Byzantine nodes.
    Finally, at the moment, the prototype assumes reliable delivery of
    messages between correct nodes.

---
+++ pkg/availability/multisigcollector/internal/parts/certverification/certverification.go
@@ -0,0 +1,108 @@
+package certverification
+
+import (
+	"errors"
+	"fmt"
+	adsl "github.com/filecoin-project/mir/pkg/availability/dsl"
+	"github.com/filecoin-project/mir/pkg/availability/multisigcollector/internal/common"
+	"github.com/filecoin-project/mir/pkg/dsl"
+	apb "github.com/filecoin-project/mir/pkg/pb/availabilitypb"
+	"github.com/filecoin-project/mir/pkg/pb/availabilitypb/mscpb"
+	t "github.com/filecoin-project/mir/pkg/types"
+	"github.com/filecoin-project/mir/pkg/util/sliceutil"
+)
+
+// IncludeVerificationOfCertificates registers event handlers for processing availabilitypb.VerifyCert events.
+func IncludeVerificationOfCertificates(
+	m dsl.Module,
+	mc *common.ModuleConfig,
+	params *common.ModuleParams,
+	nodeID t.NodeID,
+	_ *common.State, // this part of the protocol is stateless.
+) {
+	// When receive a request to verify a certificate, check that it is structurally correct and verify the signatures.
+	adsl.UponVerifyCert(m, func(cert *apb.Cert, origin *apb.VerifyCertOrigin) error {
+		mscCert, err := verifyCertificateStructure(params, cert)
+		if err != nil {
+			adsl.CertVerified(m, t.ModuleID(origin.Module), err, origin)
+			return nil
+		}
+
+		sigMsg := common.SigData(params.InstanceUID, t.BatchID(mscCert.BatchId))
+		dsl.VerifyNodeSigs(m, mc.Crypto,
+			/*data*/ sliceutil.Repeat(sigMsg, len(mscCert.Signers)),
+			/*signatures*/ mscCert.Signatures,
+			/*nodeIDs*/ t.NodeIDSlice(mscCert.Signers),
+			/*context*/ &verifySigsInCertContext{origin},
+		)
+		return nil
+	})
+
+	// When the signatures in a certificate are verified, output the result of certificate verification.
+	dsl.UponNodeSigsVerified(m, func(nodeIDs []t.NodeID, errs []error, allOK bool, context *verifySigsInCertContext) error {
+		var err error
+		if !allOK {
+			err = errors.New("some signatures are invalid")
+		}
+
+		adsl.CertVerified(m, t.ModuleID(context.origin.Module), err, context.origin)
+		return nil
+	})
+}
+
+// verifyCertificateStructure checks that the certificate is well-formed without checking the validity of the
+// cryptographic authenticators like hashes and digital signatures.
+func verifyCertificateStructure(params *common.ModuleParams, cert *apb.Cert) (*mscpb.Cert, error) {
+	// Check that the certificate is present.
+	if cert == nil || cert.Type == nil {
+		return nil, fmt.Errorf("the certificate is nil")
+	}
+
+	// Check that the certificate is of the right type.
+	mscCertWrapper, ok := cert.Type.(*apb.Cert_Msc)
+	if !ok {
+		return nil, fmt.Errorf("unexpected certificate type")
+	}
+	mscCert := mscCertWrapper.Msc
+
+	// Check that the certificate contains a sufficient number of signatures.
+	if len(mscCert.Signers) <= (params.N()+params.F())/2 {
+		return nil, fmt.Errorf("insuficient number of signatures")
+	}
+
+	if len(mscCert.Signers) != len(mscCert.Signatures) {
+		return nil, fmt.Errorf("the number of signatures does not correspond to the number of signers")
+	}
+
+	// Check that the identities of the signing nodes are not repeated.
+	alreadySeen := make(map[t.NodeID]struct{})
+	for _, idRaw := range mscCert.Signers {
+		id := t.NodeID(idRaw)
+		if _, ok := alreadySeen[id]; ok {
+			return nil, fmt.Errorf("some node ids in the certificate are repeated multiple times")
+		}
+		alreadySeen[id] = struct{}{}
+	}
+
+	// Check that the identities of the source node and the signing nodes are valid.
+	allNodes := make(map[t.NodeID]struct{})
+	for _, id := range params.AllNodes {
+		allNodes[id] = struct{}{}
+	}
+
+	for _, idRaw := range mscCert.Signers {
+		if _, ok := allNodes[t.NodeID(idRaw)]; !ok {
+			return nil, fmt.Errorf("unknown node id: %v", t.NodeID(idRaw))
+		}
+	}
+
+	return mscCert, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Context data structures                                                                                            //
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+type verifySigsInCertContext struct {
+	origin *apb.VerifyCertOrigin
+}
--- protos/generate.go
+++ protos/generate.go
@@ -21,6 +21,7 @@ package protos
 //go:generate protoc-events dslpb/dslpb.proto
 //go:generate protoc-events mempoolpb/mempoolpb.proto
 //go:generate protoc-events availabilitypb/availabilitypb.proto
+//go:generate protoc-events availabilitypb/mscpb/mscpb.proto
 
 //go:generate protoc --proto_path=. --go_out=:../pkg/ --go_opt=paths=source_relative simplewal/simplewal.proto
 //go:generate protoc --proto_path=. --go_out=:../samples/ --go_opt=paths=source_relative chat-demo/chatdemo.proto
