db45d018277d5bac53f981bb51945536fa14fd9f
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2022-07-13 12:36:02 +0200

    Clean up net.Transport

--- pkg/net/transport.go
+++ pkg/net/transport.go
@@ -2,18 +2,18 @@ package net
 
 import (
 	"context"
+	"github.com/filecoin-project/mir/pkg/modules"
 
-	"github.com/filecoin-project/mir/pkg/events"
 	"github.com/filecoin-project/mir/pkg/pb/messagepb"
 	t "github.com/filecoin-project/mir/pkg/types"
 )
 
 type Transport interface {
+	modules.ActiveModule
+
 	Start() error
 	Stop()
-	ImplementsModule()
-	EventsOut() <-chan *events.EventList
-	ApplyEvents(ctx context.Context, eventList *events.EventList) error
+
 	Send(dest t.NodeID, msg *messagepb.Message) error
 	Connect(ctx context.Context)
 }
