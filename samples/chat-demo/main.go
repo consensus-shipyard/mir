consensus-shipyard/mir

commit 28f4ea686ac8a851cf9d758e7da33a6f39c8193f

Libp2p transport refactoring
Cleaned up the code a bit and made the API for testing a bit more
general by removing assumptions about node ids being integers.

samples/chat-demo/main.go
@@ -18,6 +18,7 @@ import (
 	"context"
 	"crypto"
 	"fmt"
+	"github.com/multiformats/go-multiaddr"
 	"os"
 	"strconv"
 	"strings"
@@ -119,11 +120,6 @@ func run() error {
 		reqReceiverAddrs[t.NewNodeIDFromInt(i)] = fmt.Sprintf("127.0.0.1:%d", reqReceiverBasePort+i)
 	}
 
-	// Generate addresses and ports of participating nodes.
-	// All nodes are on the local machine, but listen on different port numbers.
-	// Change this or make this configurable do deploy different nodes on different physical machines.
-	nodeAddrs := make(map[t.NodeID]string)
-
 	// ================================================================================
 	// Create and initialize various modules used by mir.
 	// ================================================================================
@@ -131,19 +127,23 @@ func run() error {
 	// Initialize the networking module.
 	// Mir will use it for transporting nod-to-node messages.
 
+	// In the current implementation, all nodes are on the local machine, but listen on different port numbers.
+	// Change this or make this configurable to deploy different nodes on different physical machines.
 	var transport net.Transport
 	switch strings.ToLower(args.Net) {
 	case "grpc":
-		transport = grpc.NewTransport(nodeAddrs, args.OwnID, logger)
+		nodeAddrs := make(map[t.NodeID]string)
 		for i := range nodeIds {
 			nodeAddrs[t.NewNodeIDFromInt(i)] = fmt.Sprintf("127.0.0.1:%d", nodeBasePort+i)
 		}
+		transport = grpc.NewTransport(nodeAddrs, args.OwnID, logger)
 	case "libp2p":
 		h := libp2ptools.NewDummyHost(ownID, nodeBasePort)
-		transport = libp2p.NewTransport(h, nodeAddrs, args.OwnID, logger)
+		nodeAddrs := make(map[t.NodeID]multiaddr.Multiaddr)
 		for i := range nodeIds {
-			nodeAddrs[t.NewNodeIDFromInt(i)] = libp2ptools.NewDummyHostID(i, nodeBasePort).String()
+			nodeAddrs[t.NewNodeIDFromInt(i)], _ = libp2ptools.NewDummyHostID(i, nodeBasePort)
 		}
+		transport = libp2p.NewTransport(h, nodeAddrs, args.OwnID, logger)
 	default:
 		return fmt.Errorf("unknown network transport %s", strings.ToLower(args.Net))
 	}

pkg/deploytest/locallibp2ptransport.go
@@ -1,45 +1,52 @@
 package deploytest
 
 import (
+	"fmt"
 	"github.com/filecoin-project/mir/pkg/logging"
 	"github.com/filecoin-project/mir/pkg/net"
 	"github.com/filecoin-project/mir/pkg/net/libp2p"
 	t "github.com/filecoin-project/mir/pkg/types"
 	libp2ptools "github.com/filecoin-project/mir/pkg/util/libp2p"
-	"strconv"
+	"github.com/libp2p/go-libp2p-core/host"
+	"github.com/multiformats/go-multiaddr"
 )
 
 type LocalLibp2pTransport struct {
 	// Complete static membership of the system.
-	// Maps the node ID of each node in the system to a string representation of its libp2p address.
-	membership map[t.NodeID]string
+	// Maps the node ID of each node in the system to its libp2p address.
+	membership map[t.NodeID]multiaddr.Multiaddr
+
+	// Maps node ids to their libp2p host.
+	hosts map[t.NodeID]host.Host
 
 	// Logger is used for all logging events of this LocalGrpcTransport
 	logger logging.Logger
 }
 
 func NewLocalLibp2pTransport(nodeIDs []t.NodeID, logger logging.Logger) *LocalLibp2pTransport {
-	membership := make(map[t.NodeID]string, len(nodeIDs))
-	for i, id := range nodeIDs {
-		membership[id] = libp2ptools.NewDummyHostID(i, BaseListenPort).String()
+	lt := &LocalLibp2pTransport{
+		membership: make(map[t.NodeID]multiaddr.Multiaddr, len(nodeIDs)),
+		hosts:      make(map[t.NodeID]host.Host),
+		logger:     logger,
 	}
 
-	return &LocalLibp2pTransport{membership, logger}
+	for i, id := range nodeIDs {
+		lt.hosts[id] = libp2ptools.NewDummyHost(i, BaseListenPort)
+		lt.membership[id] = libp2ptools.NewDummyPeerID(i, BaseListenPort)
+	}
+
+	return lt
 }
 
-func (t *LocalLibp2pTransport) Link(sourceID t.NodeID) net.Transport {
-	// TODO: it should be possible for sourceID to be an arbitrary string
-	id, err := strconv.Atoi(string(sourceID))
-	if err != nil {
-		panic(err)
+func (lt *LocalLibp2pTransport) Link(sourceID t.NodeID) net.Transport {
+	if _, ok := lt.hosts[sourceID]; !ok {
+		panic(fmt.Errorf("unexpected node id: %v", sourceID))
 	}
 
-	h := libp2ptools.NewDummyHost(id, BaseListenPort)
-
 	return libp2p.NewTransport(
-		h,
-		t.membership,
+		lt.hosts[sourceID],
+		lt.membership,
 		sourceID,
-		t.logger,
+		lt.logger,
 	)
 }

pkg/net/libp2p/libp2ptransport.go
@@ -41,14 +41,14 @@ var _ mirnet.Transport = &Transport{}
 type Transport struct {
 	host              host.Host
 	ownID             types.NodeID
-	membership        map[types.NodeID]string
+	membership        map[types.NodeID]multiaddr.Multiaddr
 	incomingMessages  chan *events.EventList
 	outboundStreamsMx sync.Mutex
 	outboundStreams   map[types.NodeID]network.Stream
 	logger            logging.Logger
 }
 
-func NewTransport(h host.Host, membership map[types.NodeID]string, ownID types.NodeID, logger logging.Logger) *Transport {
+func NewTransport(h host.Host, membership map[types.NodeID]multiaddr.Multiaddr, ownID types.NodeID, logger logging.Logger) *Transport {
 	if logger == nil {
 		logger = logging.ConsoleErrorLogger
 	}
@@ -109,23 +109,16 @@ func (t *Transport) Connect(ctx context.Context) {
 			continue
 		}
 
-		go func(nodeID types.NodeID, nodeAddr string) {
+		go func(nodeID types.NodeID, nodeAddr multiaddr.Multiaddr) {
 			defer wg.Done()
 
 			t.logger.Log(logging.LevelDebug, fmt.Sprintf("node %s is connecting to node %s", t.ownID.Pb(), nodeID.Pb()))
 
-			maddr, err := multiaddr.NewMultiaddr(nodeAddr)
-			if err != nil {
-				t.logger.Log(logging.LevelError,
-					fmt.Sprintf("failed to parse %v for node %v: %v", nodeAddr, nodeID, err))
-				return
-			}
-
 			// Extract the peer ID from the multiaddr.
-			info, err := peer.AddrInfoFromP2pAddr(maddr)
+			info, err := peer.AddrInfoFromP2pAddr(nodeAddr)
 			if err != nil {
 				t.logger.Log(logging.LevelError,
-					fmt.Sprintf("failed to parse addr %v for node %v: %v", maddr, nodeID, err))
+					fmt.Sprintf("failed to parse addr %v for node %v: %v", nodeAddr, nodeID, err))
 				return
 			}
 

pkg/util/libp2p/libp2p.go
@@ -2,31 +2,17 @@ package libp2p
 
 import (
 	"fmt"
-	mrand "math/rand"
-
 	"github.com/libp2p/go-libp2p"
 	libp2pcrypto "github.com/libp2p/go-libp2p-core/crypto"
 	"github.com/libp2p/go-libp2p-core/host"
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/multiformats/go-multiaddr"
+	mrand "math/rand"
 )
 
 // NewDummyHost creates an insecure libp2p host for test and demonstration purposes.
 func NewDummyHost(id, basePort int) host.Host {
-	rand := mrand.New(mrand.NewSource(int64(id))) // nolint
-
-	priv, _, err := libp2pcrypto.GenerateKeyPairWithReader(libp2pcrypto.Ed25519, -1, rand)
-	if err != nil {
-		panic(err)
-	}
-	if err != nil {
-		panic(err)
-	}
-
-	sourceMultiAddr, err := multiaddr.NewMultiaddr(fmt.Sprintf("/ip4/127.0.0.1/tcp/%d", basePort+id))
-	if err != nil {
-		panic(err)
-	}
+	sourceMultiAddr, priv := NewDummyHostID(id, basePort)
 
 	h, err := libp2p.New(
 		// Use the keypair we generated
@@ -42,8 +28,8 @@ func NewDummyHost(id, basePort int) host.Host {
 	return h
 }
 
-// NewDummyHostID regenerates a libp2p host address for test and demonstration purposes.
-func NewDummyHostID(id, basePort int) multiaddr.Multiaddr {
+// NewDummyHostID generates a libp2p host address for test and demonstration purposes.
+func NewDummyHostID(id, basePort int) (multiaddr.Multiaddr, libp2pcrypto.PrivKey) {
 	rand := mrand.New(mrand.NewSource(int64(id))) // nolint
 
 	priv, _, err := libp2pcrypto.GenerateKeyPairWithReader(libp2pcrypto.Ed25519, -1, rand)
@@ -56,9 +42,12 @@ func NewDummyHostID(id, basePort int) multiaddr.Multiaddr {
 		panic(err)
 	}
 
-	if err != nil {
-		panic(err)
-	}
+	return sourceMultiAddr, priv
+}
+
+// NewDummyPeerID generates a libp2p peer address for the host generated by NewDummyHost(id, basePort).
+func NewDummyPeerID(id, basePort int) multiaddr.Multiaddr {
+	sourceMultiAddr, priv := NewDummyHostID(id, basePort)
 
 	peerID, err := peer.IDFromPrivateKey(priv)
 	if err != nil {

commit 9a142e1dee639f1faaf500c71a8fe5cc0de55f3c

Hotfix chat-demo
Fixes a bug introduced in 28f4ea6

samples/chat-demo/main.go
@@ -141,7 +141,7 @@ func run() error {
 		h := libp2ptools.NewDummyHost(ownID, nodeBasePort)
 		nodeAddrs := make(map[t.NodeID]multiaddr.Multiaddr)
 		for i := range nodeIds {
-			nodeAddrs[t.NewNodeIDFromInt(i)], _ = libp2ptools.NewDummyHostID(i, nodeBasePort)
+			nodeAddrs[t.NewNodeIDFromInt(i)] = libp2ptools.NewDummyPeerID(i, nodeBasePort)
 		}
 		transport = libp2p.NewTransport(h, nodeAddrs, args.OwnID, logger)
 	default: