ef1b43ef1f4a389a4760523ebe4a44b8ad9070f6
André Breda <andrebreda@tecnico.ulisboa.pt>
2022-09-09 19:40:18 +0100

    Document ThreshCrypto API

--- pkg/threshcrypto/api.go
+++ pkg/threshcrypto/api.go
@@ -6,24 +6,30 @@ SPDX-License-Identifier: Apache-2.0
 
 package threshcrypto
 
-// The ThreshCrypto interface represents an implementation of the cryptographic primitives inside the MirModule module.
-// It is responsible for producing and verifying cryptographic threshold signatures.
+// The ThreshCrypto interface represents an implementation of threshold cryptography primitives inside the MirModule module.
+// It is responsible for producing and verifying cryptographic threshold signatures, which disperses the authority
+// to sign among a group of N members, where T must sign their share for a full signature to be produced.
 // It internally stores information about the group's public key and the node's private key share.
 type ThreshCrypto interface {
-
 	// SignShare signs the provided data and returns the resulting signature share.
 	// The data to be signed is the concatenation of all the passed byte slices.
 	// A signature share produced by SignShare is verifiable using VerifyShare.
-	// After combining the required amount of signature shares, they can be combined into a full signature using Combine.
-	// Note that the private key used to produce the signature cannot be set ("registered") through this interface.
-	// Storing and using the private key is completely implementation-dependent.
+	// After obtaining signature shares from T group members, the full signature can be constructed with Recover.
+	// Returns the signature (and a nil error) on success, and a non-nil error otherwise.
 	SignShare(data [][]byte) ([]byte, error)
 
+	// VerifyShare verifies that a signature share is valid for the given data.
+	// Returns nil on success (i.e., if the given signature share is valid) and a non-nil error otherwise.
 	VerifyShare(data [][]byte, signatureShare []byte) error
 
+	// Recover constructs a full signature from signature shares over data.
+	// The given array may contain invalid signature shares: as long as at least T distinct signature
+	// shares are valid, Recover will succeed, albeit less efficiently.
+	// Returns the full signature (and a nil error) on success and a non-nil error otherwise.
+	// Signatures returned by Recover are guaranteed to be valid.
 	Recover(data [][]byte, signatureShares [][]byte) ([]byte, error)
 
-	// VerifyFull verifies a full signature produced by the node with ID nodeID over data.
+	// VerifyFull verifies a full signature from the group over data.
 	// Returns nil on success (i.e., if the given signature is valid) and a non-nil error otherwise.
 	VerifyFull(data [][]byte, signature []byte) error
 }
