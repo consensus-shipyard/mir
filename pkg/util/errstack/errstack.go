d4e39b129c071e927345380c16dbea608c48a117
Matej Pavlovic <matopavlovic@gmail.com>
2022-09-14 14:37:00 +0200

    Add utility for printing errors with stack traces
    This package is intended for usage with the
    "github.com/pkg/errors" package for wrapping errors.
    If errors are wrapped using methods of that package
    (not implemented yet), the output can be much more informative.
    
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

---
+++ pkg/util/errstack/errstack.go
@@ -0,0 +1,52 @@
+package errstack
+
+import (
+	"fmt"
+
+	"github.com/pkg/errors"
+)
+
+// Println prints err to standard output.
+// If err represents a chain of errors, Println prints the first error in the chain that has a stack trace.
+func Println(err error) {
+	fmt.Println(err)
+	firstWithStack := FirstWithStack(err)
+	if hasStack(firstWithStack) {
+		fmt.Printf("First error with a stack trace:\n%v%+v\n", firstWithStack, firstWithStack.(stackTracer).StackTrace())
+	}
+}
+
+// FirstWithStack returns the first error in the chain of errors that has a stack trace associated with it.
+// The chain of errors is determined by repeatedly calling Unwrap() on the err.
+// "First" is defined as the error that is returned by the last call to Unwrap().
+func FirstWithStack(err error) error {
+	if !isWrapper(err) {
+		return err
+	}
+
+	earlier := FirstWithStack(errors.Unwrap(err))
+	if hasStack(earlier) {
+		return earlier
+	}
+
+	return err
+}
+
+// isWrapper returns true if err implements the Unwrap() method.
+func isWrapper(err error) bool {
+	type wrapper interface {
+		Unwrap() error
+	}
+	_, ok := err.(wrapper)
+	return ok
+}
+
+// hasStack returns true if err implements the StackTrace() method.
+func hasStack(err error) bool {
+	_, ok := err.(stackTracer)
+	return ok
+}
+
+type stackTracer interface {
+	StackTrace() errors.StackTrace
+}
