fd4f0a330f7a907476d4c5c551b1a7bcb60feb0e
Sergey Fedorov <sergey.fedorov@protocol.ai>
2022-09-19 16:12:04 +0200

    cmd/bench: Switch to using SMR system abstraction

--- cmd/bench/cmd/app.go
+++ cmd/bench/cmd/app.go
@@ -7,51 +7,34 @@ package cmd
 import (
 	"fmt"
 
-	"github.com/filecoin-project/mir/pkg/events"
 	"github.com/filecoin-project/mir/pkg/logging"
-	"github.com/filecoin-project/mir/pkg/modules"
-	bfpb "github.com/filecoin-project/mir/pkg/pb/batchfetcherpb"
-	"github.com/filecoin-project/mir/pkg/pb/eventpb"
+	"github.com/filecoin-project/mir/pkg/pb/commonpb"
+	"github.com/filecoin-project/mir/pkg/pb/requestpb"
 	t "github.com/filecoin-project/mir/pkg/types"
+	"github.com/filecoin-project/mir/pkg/util/maputil"
 )
 
 type App struct {
 	logging.Logger
 
-	ProtocolModule t.ModuleID
-	Membership     map[t.NodeID]t.NodeAddress
+	Membership map[t.NodeID]t.NodeAddress
 }
 
-func (App) ImplementsModule() {}
-
-func (a *App) ApplyEvents(eventsIn *events.EventList) (*events.EventList, error) {
-	return modules.ApplyEventsSequentially(eventsIn, a.ApplyEvent)
-}
-
-func (a *App) ApplyEvent(event *eventpb.Event) (*events.EventList, error) {
-	switch e := event.Type.(type) {
-	case *eventpb.Event_Init:
-	case *eventpb.Event_BatchFetcher:
-		switch e := e.BatchFetcher.Type.(type) {
-		case *bfpb.Event_NewOrderedBatch:
-			for _, req := range e.NewOrderedBatch.Txs {
-				a.Log(logging.LevelDebug, fmt.Sprintf("Delivered request %v from client %v", req.ReqNo, req.ClientId))
-			}
-		default:
-			return nil, fmt.Errorf("unexpected availability event type: %T", e)
-		}
-	case *eventpb.Event_AppSnapshotRequest:
-		return events.ListOf(events.AppSnapshotResponse(
-			t.ModuleID(e.AppSnapshotRequest.Module),
-			[]byte{},
-			e.AppSnapshotRequest.Origin,
-		)), nil
-	case *eventpb.Event_AppRestoreState:
-	case *eventpb.Event_NewEpoch:
-		return events.ListOf(events.NewConfig(a.ProtocolModule, a.Membership)), nil
-	default:
-		return nil, fmt.Errorf("unexpected type of App event: %T", event.Type)
+func (a *App) ApplyTXs(txs []*requestpb.Request) error {
+	for _, req := range txs {
+		a.Log(logging.LevelDebug, fmt.Sprintf("Delivered request %v from client %v", req.ReqNo, req.ClientId))
 	}
+	return nil
+}
 
-	return events.EmptyList(), nil
+func (a *App) NewEpoch(_ t.EpochNr) (map[t.NodeID]t.NodeAddress, error) {
+	return maputil.Copy(a.Membership), nil
+}
+
+func (a *App) Snapshot() ([]byte, error) {
+	return nil, nil
+}
+
+func (a *App) RestoreState(appData []byte, epochConfig *commonpb.EpochConfig) error {
+	return nil
 }
