db377692f1e9310f02d54d28fc81354015db6c29
Denis Kolegov <dnkolegov@gmail.com>
2022-09-27 12:17:34 +0300

    Introduce fuzzing (#232)
    * Introduce fuzzing
    
    * fix FuzzSnapshotForHash

---
+++ pkg/serializing/serializing_fuzz_test.go
@@ -0,0 +1,46 @@
+package serializing
+
+import (
+	"fmt"
+	"strconv"
+	"testing"
+
+	"github.com/filecoin-project/mir/pkg/pb/commonpb"
+	"github.com/filecoin-project/mir/pkg/pb/requestpb"
+	"github.com/filecoin-project/mir/pkg/types"
+)
+
+func FuzzRequestForHash(f *testing.F) {
+	f.Add("/ip4/7.7.7.7/tcp/1234/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N", 0, 1, []byte("13242342342342"))
+
+	f.Fuzz(func(t *testing.T, id string, tp, n int, data []byte) {
+		req := requestpb.Request{ClientId: id, ReqNo: uint64(n), Type: uint64(tp), Data: data}
+		RequestForHash(&req)
+	})
+}
+
+func FuzzCheckpointForSig(f *testing.F) {
+	f.Add(uint64(0), uint64(0), []byte("13242342342342"))
+
+	f.Fuzz(func(t *testing.T, s, n uint64, data []byte) {
+		CheckpointForSig(types.EpochNr(s), types.SeqNr(n), data)
+	})
+}
+
+func FuzzSnapshotForHash(f *testing.F) {
+	f.Add(100, uint64(0), "/ip4/7.7.7.7/tcp/1234/p2p/QmYyQSo1c1Ym7orWxLYvCrM2EmxFTANf8wXmmE7DWjhx5N", "127.0.0.1:3333", []byte("13242342342342"))
+
+	f.Fuzz(func(t *testing.T, n int, e uint64, k, v string, data []byte) {
+		n = n % 5000
+		membership := make(map[string]string)
+
+		for i := 0; i < n; i++ {
+			membership[fmt.Sprintf("%s/%s", k, strconv.Itoa(i))] = fmt.Sprintf("%s%s", v, strconv.Itoa(i))
+		}
+
+		mb := commonpb.Membership{Membership: membership}
+		cfg := commonpb.EpochConfig{EpochNr: e, Memberships: []*commonpb.Membership{&mb}}
+		state := commonpb.StateSnapshot{AppData: data, Configuration: &cfg}
+		SnapshotForHash(&state)
+	})
+}
