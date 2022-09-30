04ac63594eea16256ab5793278144d91311b261d
Matej Pavlovic <matopavlovic@gmail.com>
2022-09-30 14:09:01 +0200

    Add test for event backpressure
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- node_test.go
+++ node_test.go
@@ -2,18 +2,23 @@ package mir
 
 import (
 	"context"
+	"fmt"
 	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
 	"github.com/golang/mock/gomock"
+	"github.com/pkg/errors"
 	"github.com/stretchr/testify/assert"
 
 	"github.com/filecoin-project/mir/pkg/events"
 	"github.com/filecoin-project/mir/pkg/logging"
 	"github.com/filecoin-project/mir/pkg/modules"
 	"github.com/filecoin-project/mir/pkg/modules/mockmodules"
+	"github.com/filecoin-project/mir/pkg/pb/eventpb"
 	"github.com/filecoin-project/mir/pkg/types"
+	"github.com/filecoin-project/mir/pkg/util/sliceutil"
 )
 
 func TestNode_Run(t *testing.T) {
@@ -81,4 +86,128 @@ func TestNode_Run(t *testing.T) {
 			<-nodeStopped
 		})
 	}
+}
+
+func TestNode_Backpressure(t *testing.T) {
+
+	nodeConfig := DefaultNodeConfig()
+
+	// Set an input event rate that would fill the node's event buffers in one second in 10 batches.
+	blabberModule := newBlabber(uint64(nodeConfig.PauseInputThreshold/10), 100*time.Millisecond)
+
+	// Set the event consumption rate to 1/2 of the input rate (i.e., draining the buffer in 2 seconds)
+	// and create the consumer module.
+	consumerDelay := 2 * time.Second / time.Duration(nodeConfig.PauseInputThreshold)
+	consumerModule := newConsumer(consumerDelay)
+
+	// Instantiate node with a fast blabber module and a slow consumer module.
+	n, err := NewNode(
+		"testnode",
+		DefaultNodeConfig(),
+		map[types.ModuleID]modules.Module{
+			"blabber":  blabberModule,
+			"consumer": consumerModule,
+		},
+		nil,
+		nil,
+	)
+	assert.Nil(t, err)
+
+	// Start a flood of dummy events.
+	blabberModule.Go()
+
+	// Start the node.
+	nodeError := make(chan error)
+	go func() {
+		nodeError <- n.Run(context.Background())
+	}()
+
+	// Run for 5 seconds, then stop the node.
+	time.Sleep(5 * time.Second)
+	n.Stop()
+	err = <-nodeError
+	fmt.Printf("node error: %v\n", err)
+	assert.True(t, errors.Is(err, ErrStopped), "unexpected node error: \"%v\", expected \"%v\"", err, ErrStopped)
+
+	// The number of submitted events must not exceed the number of consumed events by too much
+	// (accounting for events still in the buffer and the overshooting caused by batched adding of events).
+	fmt.Printf("Total submitted events: %d\n", atomic.LoadUint64(&blabberModule.totalSubmitted))
+	totalSubmitted := atomic.LoadUint64(&blabberModule.totalSubmitted)
+	expectSubmitted := atomic.LoadUint64(&consumerModule.numProcessed) +
+		uint64(nodeConfig.PauseInputThreshold) + // Events left in the buffer
+		uint64(nodeConfig.MaxEventBatchSize) + // Events in the consumer's processing queue
+		2*blabberModule.batchSize // one batch of overshooting, one batch waiting in the babbler's output channel.
+	assert.LessOrEqual(t, totalSubmitted, expectSubmitted, "too many events submitted (node event buffer overflow)")
+
+}
+
+// =================================================================================================
+// Blabber module (for testing event backpressure)
+// =================================================================================================
+
+// The babbler is a simple ActiveModule that just produces batches of dummy events at a given rate.
+type blabber struct {
+	flood          chan *events.EventList // Output channel for batches of dummy events.
+	batchSize      uint64                 // Number of events output at once.
+	period         time.Duration          // Time between batches.
+	totalSubmitted uint64                 // Counter for total number of submitted events.
+}
+
+func newBlabber(batchSize uint64, period time.Duration) *blabber {
+	return &blabber{
+		flood:     make(chan *events.EventList),
+		batchSize: batchSize,
+		period:    period,
+	}
+}
+
+// Go starts babbling, i.e., producing batches of dummy events at the rate configured at instantiation.
+// Once started, the babbler is unstoppable and keeps babbling forever.
+func (b *blabber) Go() {
+	go func() {
+		for {
+			evts := events.ListOf(sliceutil.Repeat(events.TestingUint("consumer", 0), int(b.batchSize))...)
+			b.flood <- evts
+			atomic.AddUint64(&b.totalSubmitted, b.batchSize)
+			time.Sleep(b.period)
+		}
+	}()
+}
+
+func (b *blabber) ImplementsModule() {}
+
+func (b *blabber) ApplyEvents(_ context.Context, _ *events.EventList) error {
+	return nil
+}
+
+func (b *blabber) EventsOut() <-chan *events.EventList {
+	return b.flood
+}
+
+// =================================================================================================
+// Consumer module (for testing event backpressure)
+// =================================================================================================
+
+// The consumer is a simple passive module that consumes events at a given rate.
+// It treats each event as a noop and just counts the number of events it has processed.
+type consumer struct {
+	delay        time.Duration
+	numProcessed uint64
+}
+
+func newConsumer(delay time.Duration) *consumer {
+	return &consumer{delay: delay}
+}
+
+func (c *consumer) ImplementsModule() {}
+
+// ApplyEvents increments a counter and sleeps for a given duration (set at module instantiation)
+// for each event in the given list.
+func (c *consumer) ApplyEvents(evts *events.EventList) (*events.EventList, error) {
+	evtsOut, err := modules.ApplyEventsSequentially(evts, func(event *eventpb.Event) (*events.EventList, error) {
+		atomic.AddUint64(&c.numProcessed, 1)
+		time.Sleep(c.delay)
+		return events.EmptyList(), nil
+	})
+	return evtsOut, err
 }
