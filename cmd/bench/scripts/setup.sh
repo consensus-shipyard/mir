5f1db75ef393417a0eeba81d027173ff8a03d165
Matej Pavlovic <matopavlovic@gmail.com>
2022-10-13 01:17:42 +0200

    Ansible playbook to setup remote benchmark
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

---
+++ cmd/bench/scripts/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+user=$(whoami)
+
+# Install Go dependencies for code generation
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install github.com/golang/mock/mockgen@v1.6.0
+
+# Load .profile (for the PATH variable)
+source /home/$user/.profile
+
+# Add go binaries to PATH if not already there
+if [[ ":$PATH:" == *":/home/$user/go/bin:"* ]]; then
+  echo "PATH already contains /home/$user/go/bin"
+else
+  echo "PATH=\$PATH:/home/$user/go/bin" >> /home/$user/.profile
+fi
