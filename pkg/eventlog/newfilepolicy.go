4d8933414fbdd37a60af825dd7e60be3ef93b56c
Alejandro Ranchal Pedrosa <a.ranchalpedrosa@gmail.com>
2022-12-28 16:29:33 +0100

    Add function to split logging on specific event tracked

--- pkg/eventlog/newfilepolicy.go
+++ pkg/eventlog/newfilepolicy.go
@@ -2,7 +2,49 @@ package eventlog
 
 import (
 	"github.com/filecoin-project/mir/pkg/events"
+	"github.com/filecoin-project/mir/pkg/pb/eventpb"
 )
+
+// Returns a file that splits an eventTime slice into multiple slices
+// every time a an event eventpb.Event_NewLogFile is found
+func EventNewLogFileLogger() func(time EventTime) *[]EventTime {
+	return EventTrackerLogger(func(event eventpb.Event) bool {
+		_, ok := event.Type.(*eventpb.Event_NewLogFile)
+		return ok
+	})
+}
+
+// eventTrackerLogger returns a function that tracks every single event of EventTime and
+// creates a new file for every event such that newFile(event) = True
+func EventTrackerLogger(newFile func(event eventpb.Event) bool) func(time EventTime) *[]EventTime {
+	return func(eventTime EventTime) *[]EventTime {
+		var result []EventTime
+		// Create a variable to hold the current chunk
+		currentChunk := &EventTime{
+			Time:   eventTime.Time,
+			Events: events.EmptyList(),
+		}
+
+		for _, event := range eventTime.Events.Slice() {
+			if newFile(*event) {
+				result = append(result, *currentChunk)
+				currentChunk = &EventTime{
+					Time:   eventTime.Time,
+					Events: events.EmptyList(),
+				}
+			} else {
+				currentChunk.Events.PushBack(event)
+			}
+		}
+
+		// If there is a remaining chunk with fewer than the desired number of events, append it to the result
+		if currentChunk.Events.Len() > 0 {
+			result = append(result, *currentChunk)
+		}
+
+		return &result
+	}
+}
 
 // EventLimitLogger returns a function for the interceptor that splits the logging file
 // every eventLimit number of events
--- pkg/requestreceiver/requestreceiver_grpc.pb.go
+++ pkg/requestreceiver/requestreceiver_grpc.pb.go
@@ -4,12 +4,10 @@ package requestreceiver
 
 import (
 	context "context"
-
+	requestpb "github.com/filecoin-project/mir/pkg/pb/requestpb"
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
-
-	requestpb "github.com/filecoin-project/mir/pkg/pb/requestpb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
