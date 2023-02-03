7a0701bca58617440720586620999ae62459fc28
Matej Pavlovic <matopavlovic@gmail.com>
2023-02-03 12:26:15 +0100

    Enable custom event transformation in mircat
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- cmd/mircat/display.go
+++ cmd/mircat/display.go
@@ -65,8 +65,9 @@ func displayEvents(args *arguments) error {
 
 				_, validEvent := args.selectedEventNames[eventName(event)]
 				_, validDest := args.selectedEventDests[event.DestModule]
+				event = customTransform(event) // Apply custom transformation to event.
 
-				if validEvent && validDest && index >= args.offset && (args.limit == 0 || index < args.offset+args.limit) {
+				if validEvent && validDest && event != nil && index >= args.offset && (args.limit == 0 || index < args.offset+args.limit) {
 					// If event type has been selected for displaying
 
 					switch e := event.Type.(type) {
