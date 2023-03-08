dc5675efdd7e9405f6c9c29d86ee80ab4912a705
Andrei Tonkikh <andrei.tonkikh@gmail.com>
2023-03-08 22:11:55 +0100

    Add documentation for the factory module (#369)
    Co-authored-by: Matej Pavlovic <matopavlovic@gmail.com>

--- pkg/factorymodule/params.go
+++ pkg/factorymodule/params.go
@@ -10,13 +10,17 @@ const (
 	DefaultMsgBufSize = 1024 * 1024 // 1 MB
 )
 
+// ModuleGenerator is a function that generates a passive module from a module ID and the parameters.
+// The parameters format is defined in .proto files (see file: protos/factorymodulepb/factorymodulepb.proto).
 type ModuleGenerator func(id t.ModuleID, params *factorymodulepb.GeneratorParams) (modules.PassiveModule, error)
 
+// ModuleParams contains the parameters for creating a factory module.
 type ModuleParams struct {
 	Generator  ModuleGenerator
 	MsgBufSize int
 }
 
+// DefaultParams creates a valid ModuleParams with default values.
 func DefaultParams(generator ModuleGenerator) ModuleParams {
 	return ModuleParams{
 		Generator:  generator,
