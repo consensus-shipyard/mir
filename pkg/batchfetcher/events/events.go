bfd904f65a7a8975f91d8e1ce23be59f0d1713ba
Matej Pavlovic <matopavlovic@gmail.com>
2023-04-04 14:54:53 +0200

    Clean up obsolete code in batch fetcher
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- pkg/batchfetcher/events/events.go
+++ pkg/batchfetcher/events/events.go
@@ -4,11 +4,12 @@ import (
 	bfpb "github.com/filecoin-project/mir/pkg/pb/batchfetcherpb"
 	"github.com/filecoin-project/mir/pkg/pb/commonpb"
 	"github.com/filecoin-project/mir/pkg/pb/eventpb"
-	"github.com/filecoin-project/mir/pkg/pb/requestpb"
 	t "github.com/filecoin-project/mir/pkg/types"
 )
 
-// These are specific events that are not automatically generated but are necessary for the batchfetcher to queue them and treat them in order
+// These are specific events that are not automatically generated but are necessary for the batchfetcher
+// to queue them and treat them in order.
+
 // Event creates an eventpb.Event out of an batchfetcherpb.Event.
 func Event(dest t.ModuleID, ev *bfpb.Event) *eventpb.Event {
 	return &eventpb.Event{
@@ -17,18 +18,6 @@ func Event(dest t.ModuleID, ev *bfpb.Event) *eventpb.Event {
 			BatchFetcher: ev,
 		},
 	}
-}
-
-// NewOrderedBatch notifies the application when an ordered batch containing transaction payloads
-// has been fetched from the availability layer and is ready for execution.
-func NewOrderedBatch(dest t.ModuleID, txs []*requestpb.Request) *eventpb.Event {
-	return Event(dest, &bfpb.Event{
-		Type: &bfpb.Event_NewOrderedBatch{
-			NewOrderedBatch: &bfpb.NewOrderedBatch{
-				Txs: txs,
-			},
-		},
-	})
 }
 
 func ClientProgress(dest t.ModuleID, progress *commonpb.ClientProgress) *eventpb.Event {
