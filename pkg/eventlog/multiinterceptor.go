37b3e4543bb8fd5c49f8d760c19031da4231cd26
Matej Pavlovic <matopavlovic@gmail.com>
2023-04-05 13:44:15 +0200

    Make sure Interceptor works even if it is nil
    Since the Interceptor is an interface type, it can happen that a nil
    value of a concrete type is used in the node and, consequently,
    Intercept() will be called on nil. The implementation of the concrete
    type thus must make sure that calling Intercept even on the nil value
    does not cause any problems.
    For more explanation, see https://mangatmodi.medium.com/go-check-nil-interface-the-right-way-d142776edef1
    
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- pkg/eventlog/multiinterceptor.go
+++ pkg/eventlog/multiinterceptor.go
@@ -9,6 +9,15 @@ type repeater struct {
 }
 
 func (r *repeater) Intercept(events *events.EventList) error {
+
+	// Avoid nil dereference if Intercept is called on a nil *Recorder and simply do nothing.
+	// This can happen if a pointer type to *Recorder is assigned to a variable with the interface type Interceptor.
+	// Mir would treat that variable as non-nil, thinking there is an interceptor, and call Intercept() on it.
+	// For more explanation, see https://mangatmodi.medium.com/go-check-nil-interface-the-right-way-d142776edef1
+	if r == nil {
+		return nil
+	}
+
 	for _, i := range r.interceptors {
 		if err := i.Intercept(events); err != nil {
 			return err
