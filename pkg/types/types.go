5cf3cfdc60bb215f8a65275b21a86764df2d5744
Alejandro Ranchal-Pedrosa <a.ranchalpedrosa@gmail.com>
2023-04-26 10:29:47 +1000

    Add EmptyContext type

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -77,6 +77,9 @@ func ErrorFromPb(ok bool, errStr string) error {
 	return errors.New(errStr)
 }
 
+//EmptyContext type is used by DSL Origin-Result style event handlers in order to signal that no context is needed to be shared
+type EmptyContext struct{}
+
 // ================================================================================
 // Auxiliary functions
 // ================================================================================
