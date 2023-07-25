571488d88586127de9e1c682acebcbf25980f111
Matej Pavlovic <matopavlovic@gmail.com>
2023-07-25 18:11:45 +0200

    Integrate local transaction generator with the app
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- cmd/bench/cmd/node.go
+++ cmd/bench/cmd/node.go
@@ -13,6 +13,10 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/filecoin-project/mir/pkg/localtxgenerator"
+	"github.com/filecoin-project/mir/pkg/rendezvous"
+	"github.com/filecoin-project/mir/pkg/trantor/appmodule"
+	tt "github.com/filecoin-project/mir/pkg/trantor/types"
 	es "github.com/go-errors/errors"
 	"github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr/net"
@@ -25,9 +29,6 @@ import (
 	"github.com/filecoin-project/mir/pkg/logging"
 	"github.com/filecoin-project/mir/pkg/membership"
 	libp2p2 "github.com/filecoin-project/mir/pkg/net/libp2p"
-	"github.com/filecoin-project/mir/pkg/pb/batchfetcherpb"
-	"github.com/filecoin-project/mir/pkg/pb/eventpb"
-	"github.com/filecoin-project/mir/pkg/pb/mempoolpb"
 	"github.com/filecoin-project/mir/pkg/transactionreceiver"
 	"github.com/filecoin-project/mir/pkg/trantor"
 	t "github.com/filecoin-project/mir/pkg/types"
@@ -39,8 +40,9 @@ const (
 )
 
 var (
-	statFileName string
-	statPeriod   time.Duration
+	statFileName  string
+	statPeriod    time.Duration
+	readyFileName string
 
 	nodeCmd = &cobra.Command{
 		Use:   "node",
@@ -55,6 +57,7 @@ func init() {
 	rootCmd.AddCommand(nodeCmd)
 	nodeCmd.Flags().StringVarP(&statFileName, "statFile", "o", "", "output file for statistics")
 	nodeCmd.Flags().DurationVar(&statPeriod, "statPeriod", time.Second, "statistic record period")
+	nodeCmd.Flags().StringVarP(&readyFileName, "readyFile", "r", "", "file to use for initial synchronization when ready to start the benchmark")
 }
 
 func runNode() error {
@@ -87,11 +90,14 @@ func runNode() error {
 	ownID := t.NodeID(id)
 
 	// Set Trantor parameters.
-	smrParams := trantor.DefaultParams(initialMembership)
-	smrParams.Mempool.MaxTransactionsInBatch = 1024
+	trantorParams := trantor.DefaultParams(initialMembership)
+	trantorParams.Mempool.MaxTransactionsInBatch = 1024
+	trantorParams.Iss.MaxProposeDelay = 0 * time.Millisecond
+	trantorParams.Iss.PBFTViewChangeSNTimeout = 8 * time.Second
+	trantorParams.Iss.PBFTViewChangeSegmentTimeout = time.Duration(trantorParams.Iss.SegmentLength) * trantorParams.Iss.PBFTViewChangeSNTimeout
 
 	// Assemble listening address.
-	// In this benchmark code, we always listen on tha address 0.0.0.0.
+	// In this benchmark code, we always listen on the address 0.0.0.0.
 	portStr, err := getPortStr(initialMembership.Nodes[ownID].Addr)
 	if err != nil {
 		return es.Errorf("could not parse port from own address: %w", err)
@@ -101,6 +107,8 @@ func runNode() error {
 	if err != nil {
 		return es.Errorf("could not create listen address: %w", err)
 	}
+
+	// Create libp2p host
 	h, err := libp2p.NewDummyHostWithPrivKey(
 		t.NodeAddress(libp2p.NewDummyMultiaddr(ownNumericID, listenAddr)),
 		libp2p.NewDummyHostKey(ownNumericID),
@@ -110,8 +118,9 @@ func runNode() error {
 	}
 
 	// Initialize the libp2p transport subsystem.
-	transport := libp2p2.NewTransport(smrParams.Net, ownID, h, logger)
+	transport := libp2p2.NewTransport(trantorParams.Net, ownID, h, logger)
 
+	// Instantiate the crypto module.
 	localCryptoSystem, err := deploytest.NewLocalCryptoSystem("pseudo", membership.GetIDs(initialMembership), logger)
 	if err != nil {
 		return es.Errorf("could not create a local crypto system: %w", err)
@@ -120,54 +129,56 @@ func runNode() error {
 	if err != nil {
 		return es.Errorf("could not create a local crypto module: %w", err)
 	}
-	genesisCheckpoint, err := trantor.GenesisCheckpoint([]byte{}, smrParams)
+
+	// Generate the initial checkpoint.
+	genesisCheckpoint, err := trantor.GenesisCheckpoint([]byte{}, trantorParams)
 	if err != nil {
 		return es.Errorf("could not create genesis checkpoint: %w", err)
 	}
-	benchApp, err := trantor.New(
+
+	// Create a local transaction generator. It has, at the same time, the interface of a trantor App,
+	// So it knows when transactions are delivered and can submit new ones accordingly.
+	txGenParams := localtxgenerator.DefaultModuleParams(tt.ClientID(ownID))
+	// Use this line instead of the above one to simulate submitting transactions to all nodes.
+	//txGenParams := localtxgenerator.DefaultModuleParams(t.NewClientIDFromInt(0))
+	txGenParams.BufSize = 100
+	txGenParams.Tps = 100
+	txGen := localtxgenerator.New(
+		localtxgenerator.DefaultModuleConfig(),
+		txGenParams,
+	)
+
+	// Create a Trantor instance.
+	trantorInstance, err := trantor.New(
 		ownID,
 		transport,
 		genesisCheckpoint,
 		localCrypto,
-		&App{Logger: logger, Membership: initialMembership},
-		smrParams,
+		appmodule.AppLogicFromStatic(txGen, initialMembership), // The transaction generator is also a static app.
+		trantorParams,
 		logger,
 	)
 	if err != nil {
 		return es.Errorf("could not create bench app: %w", err)
 	}
 
-	recorder, err := eventlog.NewRecorder(
-		ownID,
-		"bench-output",
-		logging.Decorate(logger, "EVTLOG: "),
-		eventlog.EventFilterOpt(func(e *eventpb.Event) bool {
-			switch e := e.Type.(type) {
-			case *eventpb.Event_Mempool:
-				switch e.Mempool.Type.(type) {
-				case *mempoolpb.Event_NewTransactions:
-					return true
-				}
-			case *eventpb.Event_BatchFetcher:
-				switch e.BatchFetcher.Type.(type) {
-				case *batchfetcherpb.Event_NewOrderedBatch:
-					return true
-				}
-			}
-			return false
-		}),
-	)
-	if err != nil {
-		return es.Errorf("cannot create event recorder: %w", err)
-	}
+	// Add transaction generator module to the setup.
+	trantorInstance.WithModule("localtxgen", txGen)
+
+	// Create recorder for gathering statistics about the performance.
 	stat := stats.NewStats()
+	statsRecorder := stats.NewStatInterceptor(stat, "app")
+
+	// Assemble the main event interceptor.
+	// We could use the statsRecorder directly, but this construction makes it convenient to add more when needed.
 	interceptor := eventlog.MultiInterceptor(
-		stats.NewStatInterceptor(stat, "app"),
-		recorder,
+		statsRecorder,
 	)
 
+	// Instantiate the Mir Node.
 	nodeConfig := mir.DefaultNodeConfig().WithLogger(logger)
-	node, err := mir.NewNode(t.NodeID(id), nodeConfig, benchApp.Modules(), interceptor)
+	nodeConfig.Stats.Period = time.Second
+	node, err := mir.NewNode(t.NodeID(id), nodeConfig, trantorInstance.Modules(), interceptor)
 	if err != nil {
 		return es.Errorf("could not create node: %w", err)
 	}
@@ -178,11 +189,32 @@ func runNode() error {
 	}
 	defer txReceiver.Stop()
 
-	if err := benchApp.Start(); err != nil {
+	if err := trantorInstance.Start(); err != nil {
 		return es.Errorf("could not start bench app: %w", err)
 	}
-	defer benchApp.Stop()
+	defer trantorInstance.Stop()
 
+	if err := transport.WaitFor(len(initialMembership.Nodes)); err != nil {
+		return es.Errorf("failed waiting for network connections: %w", err)
+	}
+
+	// Synchronize with other nodes if necessary.
+	// If invoked, this code blocks until all the nodes have connected to each other.
+	// (The file created by Ready must be deleted by some external code (or manually) after all nodes have created it.)
+	if readyFileName != "" {
+		syncCtx, cancelFunc := context.WithTimeout(ctx, 10*time.Second)
+		err = rendezvous.NewFileSyncer(readyFileName, 200*time.Millisecond).Ready(syncCtx)
+		cancelFunc()
+		if err != nil {
+			return fmt.Errorf("error synchronizing nodes: %w", err)
+		}
+	}
+
+	// Start generating the load.
+	txGen.Start()
+	defer txGen.Stop()
+
+	// Output the statistics.
 	var statFile *os.File
 	if statFileName != "" {
 		statFile, err = os.Create(statFileName)
