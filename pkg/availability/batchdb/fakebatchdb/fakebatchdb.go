b9cdb66e355b67d1d0601a0327335565da24d2a8
Matej Pavlovic <matopavlovic@gmail.com>
2023-07-26 13:18:48 +0200

    Add garbage-collection to fake batch db
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- pkg/availability/batchdb/fakebatchdb/fakebatchdb.go
+++ pkg/availability/batchdb/fakebatchdb/fakebatchdb.go
@@ -17,7 +17,9 @@ type ModuleConfig struct {
 }
 
 type moduleState struct {
-	BatchStore map[msctypes.BatchID]batch
+	batchStore      map[msctypes.BatchID]*batch
+	batchesByRetIdx map[tt.RetentionIndex][]msctypes.BatchID
+	retIdx          tt.RetentionIndex
 }
 
 type batch struct {
@@ -30,12 +32,24 @@ func NewModule(mc ModuleConfig) modules.Module {
 	m := dsl.NewModule(mc.Self)
 
 	state := moduleState{
-		BatchStore: make(map[msctypes.BatchID]batch),
+		batchStore:      make(map[msctypes.BatchID]*batch),
+		batchesByRetIdx: make(map[tt.RetentionIndex][]msctypes.BatchID),
+		retIdx:          0,
 	}
 
 	// On StoreBatch request, just store the data in the local memory.
-	batchdbpbdsl.UponStoreBatch(m, func(batchID msctypes.BatchID, txs []*trantorpbtypes.Transaction, origin *batchdbpbtypes.StoreBatchOrigin) error {
-		state.BatchStore[batchID] = batch{txs}
+	batchdbpbdsl.UponStoreBatch(m, func(
+		batchID msctypes.BatchID,
+		txs []*trantorpbtypes.Transaction,
+		retIdx tt.RetentionIndex,
+		origin *batchdbpbtypes.StoreBatchOrigin,
+	) error {
+		if retIdx >= state.retIdx {
+			b := batch{txs}
+			state.batchStore[batchID] = &b
+			state.batchesByRetIdx[retIdx] = append(state.batchesByRetIdx[retIdx], batchID)
+		}
+
 		batchdbpbdsl.BatchStored(m, origin.Module, origin)
 		return nil
 	})
@@ -43,13 +57,23 @@ func NewModule(mc ModuleConfig) modules.Module {
 	// On LookupBatch request, just check the local map.
 	batchdbpbdsl.UponLookupBatch(m, func(batchID msctypes.BatchID, origin *batchdbpbtypes.LookupBatchOrigin) error {
 
-		storedBatch, found := state.BatchStore[batchID]
+		storedBatch, found := state.batchStore[batchID]
 		if !found {
 			batchdbpbdsl.LookupBatchResponse(m, origin.Module, false, nil, origin)
 			return nil
 		}
 
 		batchdbpbdsl.LookupBatchResponse(m, origin.Module, true, storedBatch.txs, origin)
+		return nil
+	})
+
+	batchdbpbdsl.UponGarbageCollect(m, func(retentionIndex tt.RetentionIndex) error {
+		for ; state.retIdx < retentionIndex; state.retIdx++ {
+			for _, batchID := range state.batchesByRetIdx[state.retIdx] {
+				delete(state.batchStore, batchID)
+			}
+			delete(state.batchesByRetIdx, state.retIdx)
+		}
 		return nil
 	})
 
