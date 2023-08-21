78ddf28ae4b1895c7a754787e5f650859016435d
Matej Pavlovic <matopavlovic@gmail.com>
2023-08-21 09:08:05 +0200

    Treat failures more gracefully in bench deployment
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- deployment/scripts/run-experiments.sh
+++ deployment/scripts/run-experiments.sh
@@ -17,13 +17,13 @@ for exp_dir in "$dir"/[0-9][0-9][0-9][0-9]; do
     echo "Skipping experiment $exp_id. File already exists: $exp_dir/results.json"
   else
     # Run the experiment and download its raw results.
-    ansible-playbook -i "$dir/hosts" --forks "$num_hosts" --extra-vars "node_config_file=$exp_dir/$node_config_file_name exp_id=$exp_id output_dir='$exp_dir' bench_output_file=$bench_output_file" run-benchmark.yaml || exit 1
+    ansible-playbook -i "$dir/hosts" --forks "$num_hosts" --extra-vars "node_config_file=$exp_dir/$node_config_file_name exp_id=$exp_id output_dir='$exp_dir' bench_output_file=$bench_output_file" run-benchmark.yaml || touch "${exp_dir}/FAILED"
 
     # Process the results of the experiment.
-    python3 scripts/analyze-bench-output.py "$exp_id" "$exp_dir/$node_config_file_name" "$exp_dir/results.json" "$exp_dir"/data/*$node_output_file.json || exit 1
+    python3 scripts/analyze-bench-output.py "$exp_id" "$exp_dir/$node_config_file_name" "$exp_dir/results.json" "$exp_dir"/data/*$node_output_file.json
 
     # Aggregate data from this and all previous experiments in a single table.
-    python3 scripts/aggregate-data.py "$dir"/partial-results.csv "$dir"/[0-9][0-9][0-9][0-9]/results.json > /dev/null || exit 1
+    python3 scripts/aggregate-data.py "$dir"/partial-results.csv "$dir"/[0-9][0-9][0-9][0-9]/results.json > /dev/null
   fi
 done
 
