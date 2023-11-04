c4c99b9584369c97f12f3271dba8c97b528fc62e
Matej Pavlovic <matopavlovic@gmail.com>
2023-11-04 11:57:45 +0100

    Add Event interface
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

---
+++ event.go
@@ -0,0 +1,25 @@
+package mir
+
+import t "github.com/filecoin-project/mir/pkg/types"
+
+type Event interface {
+
+	// Src returns the module that emitted the event.
+	// While this information is not always necessary for the system operation,
+	// it is useful for analyzing event traces and debugging.
+	Src() t.ModuleID
+
+	// Dest returns the destination module of the event.
+	Dest() t.ModuleID
+
+	// Bytes returns a serialized representation of the event
+	// as a slice of bytes from which the event can be reconstructed.
+	// Note that Bytes does not necessarily guarantee the output to be deterministic.
+	// Even multiple subsequent calls to Bytes on the same event object might return different byte slices.
+	Bytes() []byte
+
+	// String returns a human-readable representation of the event.
+	// While not used by the runtime itself, it can be used by associated tools.
+	// Conventionally, this representation is JSON.
+	String() string
+}
