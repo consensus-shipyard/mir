500b7cf83ab5f62e529eab5e1026d9aae81325d8
Matej Pavlovic <matopavlovic@gmail.com>
2023-12-17 21:17:48 +0100

    Clean up stdevents
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- stdtypes/types.go
+++ stdtypes/types.go
@@ -1,9 +1,6 @@
 package stdtypes
 
 import (
-	"fmt"
-	"strconv"
-
 	"github.com/multiformats/go-multiaddr"
 )
 
@@ -15,45 +12,16 @@ import (
 type RetentionIndex uint64
 
 // Message represents a message to be sent over the network.
-// It is the data type of the SendMessage.Payload and MessageReceived.Payload.
+// It is the data type of the stdevents.SendMessage.Payload and stdevents.MessageReceived.Payload.
 // The only requirement of a Message is that it must be serializable.
-// Note that while a SendMessage event as well as a MessageReceived event can contain
+// Note that while a stdevents.SendMessage event as well as a stdevents.MessageReceived event can contain
 // an arbitrary implementation of this interface in their Payload fields,
-// A SendMessage or a MessageReceived event that underwent serialization and deserialization
+// a SendMessage or a MessageReceived event that underwent serialization and deserialization
 // will always contain the RawMessage implementation of this interface,
 // only holding the serialized representation of the original message.
 // This is because Mir does not by itself know how to deserialize arbitrary (application-specific) messages.
 type Message interface {
 	ToBytes() ([]byte, error)
-}
-
-type RawMessage []byte
-
-func (m RawMessage) ToBytes() ([]byte, error) {
-	return m, nil
-}
-
-func (m RawMessage) String() string {
-	if isPrintableString(m) {
-		return string(m)
-	}
-	return fmt.Sprintf("%v", []byte(m))
-}
-
-func isPrintableString(data []byte) bool {
-	for _, r := range string(data) {
-		if !strconv.IsPrint(r) {
-			return false
-		}
-	}
-	return true
-	// TODO: Replace the above by the commented code below once the appropriate version of Go is supported.
-	//   Currently we do not support such a high version of Go, because we are using an old version of libp2p
-	//   that requires an old Go version (tested with go 1.19). However, updating libp2p to a higher version
-	//   breaks the libp2p transport module. That needs to be fixed first.
-	//return !bytes.ContainsFunc(data, func(r rune) bool {
-	//	return !strconv.IsPrint(r)
-	//})
 }
 
 // NodeAddress represents the address of a node.
