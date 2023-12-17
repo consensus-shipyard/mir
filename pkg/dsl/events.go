67a31d76f74480ccb2566e6f6e78d4ce724d690e
Matej Pavlovic <matopavlovic@gmail.com>
2023-12-17 21:59:20 +0100

    Clean up DSL Origin event
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

--- pkg/dsl/events.go
+++ pkg/dsl/events.go
@@ -4,13 +4,6 @@ import (
 	dslpbtypes "github.com/filecoin-project/mir/pkg/pb/dslpb/types"
 )
 
-// Origin creates a dslpb.Origin protobuf.
-func Origin(contextID ContextID) *dslpbtypes.Origin {
-	return &dslpbtypes.Origin{
-		ContextID: contextID.Pb(),
-	}
-}
-
 // MirOrigin creates a dslpb.Origin protobuf.
 func MirOrigin(contextID ContextID) *dslpbtypes.Origin {
 	return &dslpbtypes.Origin{
