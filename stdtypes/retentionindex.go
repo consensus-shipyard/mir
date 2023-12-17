36a38b6eca1c29502d9d0d6565cb8b3b9dc733b6
Matej Pavlovic <matopavlovic@gmail.com>
2023-12-17 22:17:55 +0100

    Clean up stdtypes
    Signed-off-by: Matej Pavlovic <matopavlovic@gmail.com>

---
+++ stdtypes/retentionindex.go
@@ -0,0 +1,8 @@
+package stdtypes
+
+// RetentionIndex represents an abstract notion of system progress used in garbage collection.
+// The basic idea is to associate various parts of the system (parts of the state, even whole modules)
+// that are subject to eventual garbage collection with a retention index.
+// As the system progresses, the retention index monotonically increases
+// and parts of the system associated with a lower retention index can be garbage-collected.
+type RetentionIndex uint64
